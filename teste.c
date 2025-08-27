42a29bcc6e00e5f58f8ff61ac4dd700d9b4e525d
otavio_lb <testeotavio124@gmail.com>
2025-08-27 20:55:33 -0300

    implementando na funcao main

--- teste.c
+++ teste.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+void flamengo(){
+    printf("Flamengo");
+    printf("AAAAAAAAAAAAAAA");
+    printf("Oi otavio :3");
+}
+
 void scan(){
     printf("Bora, Jonathan. \n");
     printf("Imagine como vai ser pra ensinar isso a Breno.");
@@ -10,10 +16,10 @@ int main (){
     printf("Jozenias lindo e gostoso lenda\n");
     printf("Entra Jonathan!!!");
     printf("AAAAAAAAAAAAAA");
+    
+    flamengo;
+    scan;
+
     return 0;
 }
-void flamengo(){
-    printf("Flamengo");
-    printf("AAAAAAAAAAAAAAA");
-    printf("Oi otavio :3");
-}
+
