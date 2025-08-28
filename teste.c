d163cf1b758576e7d0c77d48c9b3885e6c88ebe7
Breno-Alves01 <brenosfg10@gmail.com>
2025-08-28 12:56:10 -0300

    alteração

--- teste.c
+++ teste.c
@@ -3,7 +3,7 @@
 int main (){
 
     printf("Aqui se inicia o projeto do JOGO EM C!\n");
-    printf("MIKE BAGUNCINHA PARA PRESIDENTE");
+    printf("JONATHAN É O CARA Q MAIS DÁ A BUNDA");
     return 0;
 }
 
